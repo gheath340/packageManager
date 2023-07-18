4b1cbfcac964e43561871f6203d0df6736dbab1b
gheath340 <heathsmail0@gmail.com>
2023-07-18 10:27:37 -0600

    add package modal implemented

--- client/src/components/addPackageModal.jsx
+++ client/src/components/addPackageModal.jsx
@@ -14,9 +14,8 @@ export function AddPackageModal({ addPackage }) {
         <>
             <button onClick={toggleModal} className="border border-gray-700 rounded-md hover:scale-110 hover:duration-200 p-1">Add Package</button>
             {modal && (
-                <div className="">
-                <div className="overlay"></div>
-                <div className="modal-content">
+            <div className="fixed flex justify-center items-center w-full h-full bg-gray-500/50">
+                <div className="flex justify-center items-center w-2/3 h-1/2 lg:h-3/5 lg:w-2/5 bg-white border border-gray-700 rounded-lg">
                     <NewPackageForm addPackage={addPackage} toggleModal={toggleModal}/>
                 </div>
             </div>
--- client/src/components/newPackageForm.jsx
+++ client/src/components/newPackageForm.jsx
@@ -4,25 +4,24 @@ import { PackageAddButton } from "./packageAddButton"
 export function NewPackageForm({ addPackage, toggleModal }) {
     
     return (
-        <>
-            <div className="flex flex-col justify-center gap-y-1">
+            <div className="flex flex-col justify-center gap-y-1 w-1/2">
                 <label>TBA</label>
-                <input placeholder="TBA" name='tba' type="text" id="tbaInput" class="border border-black rounded"></input>
+                <input placeholder="TBA" name='tba' type="text" id="tbaInput" class="border border-gray-700 rounded"></input>
                 <label>Item</label>
-                <input placeholder="Item" name='tba' type="text" id="tbaInput" class="border border-black rounded"></input>
+                <input placeholder="Item" name='tba' type="text" id="tbaInput" class="border border-gray-700 rounded"></input>
                 <label>Weight</label>
-                <input placeholder="Weight" name='tba' type="text" id="tbaInput" class="border border-black rounded"></input>
+                <input placeholder="Weight" name='tba' type="text" id="tbaInput" class="border border-gray-700 rounded"></input>
                 <label>Location</label>
-                <input placeholder="Location" name='tba' type="text" id="tbaInput" class="border border-black rounded"></input>
+                <input placeholder="Location" name='tba' type="text" id="tbaInput" class="border border-gray-700 rounded"></input>
                 <label>City</label>
-                <input placeholder="City" name='tba' type="text" id="tbaInput" class="border border-black rounded"></input>
+                <input placeholder="City" name='tba' type="text" id="tbaInput" class="border border-gray-700 rounded"></input>
                 <label>Driver ID</label>
-                <input placeholder="Driver ID" name='tba' type="text" id="tbaInput" class="border border-black rounded"></input>
+                <input placeholder="Driver ID" name='tba' type="text" id="tbaInput" class="border border-gray-700 rounded"></input>
+        
+                <div className="w-full flex flex-col items-center">
+                    <PackageAddButton addPackage={addPackage}/>
+                    <button onClick={toggleModal} className="border border-gray-700 rounded-md hover:scale-110 hover:duration-200 p-1 mt-3 w-1/2"><Link to="/packages">Cancel</Link></button>
+                </div>
             </div>
-            <div className="w-full flex flex-col items-center">
-                <PackageAddButton addPackage={addPackage}/>
-                <button onClick={toggleModal} className="border border-black rounded-md hover:scale-110 hover:duration-200 p-1 mt-3 w-1/2"><Link to="/packages">Cancel</Link></button>
-            </div>
-        </>
     )
 }
