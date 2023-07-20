ea05d6d8a5734bf233d038d0fb58a2faea4e372d
gheath340 <heathsmail0@gmail.com>
2023-07-20 13:19:18 -0600

    fixed edit modal positioning

--- client/src/components/editPackageModal.jsx
+++ client/src/components/editPackageModal.jsx
@@ -14,7 +14,7 @@ export function EditPackageModal({ editPackage }) {
         <>
             <button onClick={toggleModal} className="border border-gray-700 rounded-md hover:scale-110 hover:duration-200 p-1 w-full mt-2">Edit</button>
             {modal && (
-            <div className="fixed flex justify-center items-center w-full h-full bg-black/50">
+            <div className="absolute left-0 top-0 flex justify-center items-center w-full h-full bg-black/50">
                 <div className="flex flex-col justify-center items-center w-2/3 h-1/2 lg:h-3/5 lg:w-2/5 bg-white border border-gray-700 rounded-lg gap-y-5">
                     <h1 className="text-3xl">Edit Package</h1>
                     <EditPackageForm editPackage={editPackage} toggleModal={toggleModal}/>
