48828da60b29446c54e34cf5ca00537eb3f3f3c5
gheath340 <heathsmail0@gmail.com>
2023-08-23 13:09:11 -0600

    driverPackageList only shows when arrow is clicked

--- client/src/components/driverItem.jsx
+++ client/src/components/driverItem.jsx
@@ -1,9 +1,14 @@
 import { EditDriverModal } from "./editDriverModal";
 import { DriverPackageList } from "./driverPackageList";
 import { Arrow } from "../components/arrow"
+import { useState } from 'react'
 
 export function DriverItem({ id, driverID, packages, active, lastStop, nextStop, city, deleteDriver, editDriver, toggleOpenDriverItems, openDriverItems }) {
+    const [list, setList] = useState(false)
 
+    const toggleList = () => {
+        setList(!list)
+    }
     
     return (
         <div className="flex flex-col">
@@ -27,11 +32,11 @@ export function DriverItem({ id, driverID, packages, active, lastStop, nextStop,
                                                         nextStop={nextStop} city={city} editDriver={editDriver}/>
                 </div>
             </div>
-            <button onClick={() => toggleOpenDriverItems(driverID)} className="self-center">
+            <button onClick={() => { toggleOpenDriverItems(driverID); toggleList() }} className="self-center">
                 <Arrow className={`${openDriverItems.includes(driverID) ? "-rotate-180" : ""} 
                                     w-6 h-6 text-gray-500 self-center transition`} />
             </button>
-            <DriverPackageList />
+            <DriverPackageList packages={packages} list={list}/>
         </div>
 
     )
--- client/src/components/driverPackageList.jsx
+++ client/src/components/driverPackageList.jsx
@@ -1,34 +1,34 @@
 
+export function DriverPackageList({ packages, list }) {
 
-export function DriverPackageList({ packages }) {
-
-    
     return (
         <ul role="list" className="divide-y divide-gray-300 flex flex-col w-2/3">
-            {packages.length === 0 && "No Packages"}
             {packages.map(p => {
                 return (
-                    
-                    <li key={p._id} className="flex justify-between gap-x-6 py-5">
-                        <div className="min-w-0 flex-auto items-start">
-                            <p className="text-md font-semibold leading-6 text-gray-900">{p.tba}</p>
-                            <p className="mt-1 truncate text-sm leading-5 text-gray-500">{p.item}</p>
-                            <p className="mt-1 truncate text-sm leading-5 text-gray-500">{p.weight}</p>
-                        </div>
-                        <div className="flex-auto">
-                            <p className="text-md leading-6 text-gray-900">{p.location}</p>
-                            <div className="mt-1 items-end gap-x-1.5">
-                                <p className="text-sm leading-5 text-gray-500">{p.city}</p>
-                                <p className="text-sm leading-5 text-gray-500">{p.driverID}</p>
+                    <>
+                        {list && (
+                            <li key={p._id} className="flex justify-between gap-x-6 py-5">
+                            <div className="min-w-0 flex-auto items-start">
+                                <p className="text-md font-semibold leading-6 text-gray-900">{p.tba}</p>
+                                <p className="mt-1 truncate text-sm leading-5 text-gray-500">{p.item}</p>
+                                <p className="mt-1 truncate text-sm leading-5 text-gray-500">{p.weight}</p>
                             </div>
-                     
-                        </div>
-                    </li>
+                            <div className="flex-auto">
+                                <p className="text-md leading-6 text-gray-900">{p.location}</p>
+                                <div className="mt-1 items-end gap-x-1.5">
+                                    <p className="text-sm leading-5 text-gray-500">{p.city}</p>
+                                    <p className="text-sm leading-5 text-gray-500">{p.driverID}</p>
+                                </div>
+                        
+                                </div>
+                            </li>
+                        )}
+                    </>
                 )
             })}
         </ul> 
-
     )
 }
-
-//thinking i need to create a div instead of package item with all the p.info in the div
+//when arrow is pointing down show nothing
+//when you click arrow package list shows up
+//NEED DRIVERS PACKAGE LIST
