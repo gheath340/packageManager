8a9de6a6dea4e958f8e880e17aa794ed979dabab
Garrett Heath <heathsmail0@gmail.com>
2023-09-15 09:59:30 -0600

    driver package list presents properly

--- client/src/components/driverPackageList.jsx
+++ client/src/components/driverPackageList.jsx
@@ -1,14 +1,18 @@
 
 
 export function DriverPackageList({ packages, list }) {
-    
+
     return (
-        <ul className="divide-y divide-gray-300 flex flex-col w-2/3">
+        <>
+        <div></div>
+        {list && (
+            <ul className="flex flex-col w-2/3 self-center divide-y divide-gray-300">
             {packages.map(p => {
                 return (
-                    <>
+                    <div key={p._id} >
                         {list && (
-                            <li key={p._id} className="flex justify-between gap-x-6 py-5">
+                            <>
+                            <li className="flex justify-between gap-x-6 py-5">
                                 <div className="min-w-0 flex-auto items-start">
                                     <p className="text-md font-semibold leading-6 text-gray-900">{p.tba}</p>
                                     <p className="mt-1 truncate text-sm leading-5 text-gray-500">{p.item}</p>
@@ -23,11 +27,15 @@ export function DriverPackageList({ packages, list }) {
                         
                                 </div>
                             </li>
+                            </>
                         )}
-                    </>
+                    </div>
                 )
             })}
-        </ul> 
+        </ul>
+        )}
+        
+        </>
     )
 }
 
