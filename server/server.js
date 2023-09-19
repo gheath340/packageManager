9289bbbc63974ddc830bd7a41e104dd15fd81956
Garrett Heath <heathsmail0@gmail.com>
2023-09-19 09:10:55 -0600

    added function comments for server code

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-
+//connect to mongodb database
 mongoose.connect("mongodb://127.0.0.1:27017/packageTracker",{
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -13,21 +13,24 @@ mongoose.connect("mongodb://127.0.0.1:27017/packageTracker",{
     .then(() => console.log("db connected"))
     .catch(console.error)
 
+//get db models
 const Package = require('./models/package')
 const Driver = require('./models/driver')
 
+//get all packages
 app.get('/packages', async (req, res) => {
     const p = await Package.find()
-
     res.json(p)
 })
 
+//get package based on id
 app.get('/package/:id', async (req, res) => {
     const p = await Package.findById(req.body.id)
 
     res.json(p)
 })
 
+//get the cities of all drivers
 app.get('/driver/cities', async (req, res) => {
     const d = await Driver.find()
     //for each driver get the city property and add to list
@@ -39,6 +42,7 @@ app.get('/driver/cities', async (req, res) => {
     res.json(cities)
 })
 
+//create a new package and add it to appropriate drivers list
 app.post('/package/add', async (req, res) => {
     const p = new Package({ tba: req.body.tba, weight: req.body.weight, item: req.body.item, 
               location: req.body.location, city: req.body.city, driverID: req.body.driverID })
@@ -51,7 +55,8 @@ app.post('/package/add', async (req, res) => {
 
     res.json(p)
 })
-//if package city is updated update driver id and put it into correct driver list
+
+//update package info
 app.put('/package/update/:id', async (req, res) => {
     const p = await Package.findById(req.params.id)
     const d = await Driver.find( { driverID: req.body.driverID })
@@ -74,27 +79,26 @@ app.put('/package/update/:id', async (req, res) => {
     res.json(p)
 })
 
+//delete package from old drivers list and add it to the new one
 const updatePackageCity = async (p, driver1, req) => {
-    //update city and remove package from current driver
     driver1[0].packages = driver1[0].packages.filter(package => 
                     package._id.toString() !== req.params.id)
-    //driver1[0].save()
 
-    //update driverId and add package to new driver list
     const driver2 = await Driver.find({ driverID: req.body.newDriverID })
     driver2[0].packages.push(p)
     driver2[0].save()
     p.save()
 }
 
-const updateDriverPackages = async (p, d) => {
-    const index = d[0].packages.findIndex(package => 
+//update specific package in drivers list
+const updateDriverPackages = async (p, driver) => {
+    const index = driver[0].packages.findIndex(package => 
         package._id.toString() === p._id.toString())
     
-        d[0].packages[index] = p
-        //d[0].save()
+        driver[0].packages[index] = p
 }
 
+//delete package based on id
 app.delete('/package/delete/:id', async (req, res) => {
     const p = await Package.findByIdAndDelete(req.params.id)
     const d = await Driver.find( { driverID: req.body.driverID })
@@ -105,24 +109,28 @@ app.delete('/package/delete/:id', async (req, res) => {
     res.json(p)
 })
 
+//get all drivers
 app.get('/drivers', async (req, res) => {
     const drivers = await Driver.find()
     
     res.json(drivers)
 })
 
+//get driver based on city
 app.get('/driver/:city', async (req, res) => {
     const driver = await Driver.find({ city: req.params.city })
 
     res.json(driver)
 })
 
+//get driver based on id
 app.get('/driver/:id', async (req, res) => {
     const driver = await Driver.findById(req.body.id)
 
     res.json(driver)
 })
 
+//create new driver
 app.post('/driver/add', (req, res) => {
     const driver = new Driver({ driverID: req.body.driverID, packages: req.body.packages, 
         active: req.body.active, lastStop: req.body.lastStop, nextStop: req.body.nextStop, 
@@ -132,6 +140,7 @@ app.post('/driver/add', (req, res) => {
     res.json(driver)
 })
 
+//update driver info
 app.put('/driver/update/:id', async (req, res) => {
     const driver = await Driver.find({ driverID: req.params.id })
     driver[0].driverID = req.body.driverID
@@ -142,6 +151,7 @@ app.put('/driver/update/:id', async (req, res) => {
     res.json(driver)
 })
 
+//delete driver based on id
 app.delete('/driver/delete/:id', async (req, res) => {
     const result = await Driver.findByIdAndDelete(req.params.id)
 
